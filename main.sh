dad0428e586ab4332ed530129edfdd16e8f84813
Katzeilla <hello@katzeilla.org>
2018-04-08 02:58:45 -0400

    Allow user select network to connect

--- main.sh
+++ main.sh
@@ -17,10 +17,11 @@ show_usage()
         echo "./main [ACTION]"
         echo
         echo "ACTION:"
-        echo "stop                    Stop NodeBB-alpine"
-        echo "build                  Build NodeBB-alpine"
-        echo "debug                     Start in to bash"
-        echo "--help, -h ,help         Show this message"
+        echo "network <network_name>                   Connect to a network"
+        echo "stop                                       Stop NodeBB-alpine"
+        echo "build                                     Build NodeBB-alpine"
+        echo "debug                                        Start in to bash"
+        echo "--help, -h ,help                            Show this message"
         echo
         echo "If no [ACTION], start normally"
 }
@@ -29,12 +30,18 @@ show_usage()
         flag='--entrypoint /bin/sh'
 		echo $date 'Debug Mode'
 
-elif [[ $1 == stop ]];then
+elif [[ $1 == network ]]; then
+    if [[ $2 != "" ]]; then
+        flag="--network $2"
+        echo Connect to $2
+    fi
+
+elif [[ $1 == stop ]]; then
         echo $date 'Stoping NodeBB-alpine......'
         nodebb_stop
         exit
 
-elif [[ $1 == build ]];then
+elif [[ $1 == build ]]; then
         echo $date 'Build NodeBB-alpine:testing ......'
 	docker build . --tag nodebb-alpine:testing 
         exit
@@ -48,7 +55,6 @@ else
 fi
 
 docker run -it \
-  -p 4567:4567 \
   --mount type=bind,source=$dir/nodebb,target=/home/node/nodebb \
   --name nodebb-alpine \
   $flag \
