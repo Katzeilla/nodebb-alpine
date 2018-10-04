71b9a166ac01a54c0405b272a1a89852df358109
Katzeilla <hello@katzeilla.org>
2018-10-03 20:10:08 -0400

    Fix typo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 NodeBB_branch="v1.8.x"
 
 cd /home/node/
-echo NodeBB not found, clone $Nodebb_branch from nodebb/nodebb
+echo NodeBB not found, clone $NodeBB_branch from nodebb/nodebb
 git clone https://github.com/nodebb/nodebb -b $NodeBB_branch
 
 /home/node/nodebb/nodebb setup
