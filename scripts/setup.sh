a1c5d3530c8b77f54315546c26878451e8ea5cdb
Katzeilla <hello@katzeilla.org>
2018-10-04 04:14:10 -0400

    Upgrade NodeBB from 1.8.x to 1.10.x

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 
 cd /home/node/ || exit
 
-NodeBB_branch="v1.8.x"
+NodeBB_branch="v1.10.x"
 
 echo NodeBB not found, clone "$NodeBB_branch" from nodebb/nodebb
 git clone https://github.com/nodebb/nodebb -b "$NodeBB_branch"
